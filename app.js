4f91b7d00136f0d10a2064817d09bd2835568035
Eshpolatov Miraziz <mirazizbek.uzb@gmail.com>
2026-06-09 13:02:47 +0500

    ishlidgn boldi

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-let mycan = (son1, son2, op) => {};
+let mycan = (son1, son2, op) => {
+  if (op === "+") {
+    return son1 + son2;
+  } else if (op === "-") {
+    return son1 - son2;
+  } else if (op === "*") {
+    return son1 * son2;
+  } else if (op === "/") {
+    return son1 / son2;
+  } else {
+    return "eroor";
+  }
+};
+console.log(mycan(10, 15, "+"));
+console.log(mycan(10, 15, "-"));
+console.log(mycan(10, 15, "*"));
+console.log(mycan(10, 15, "/"));
